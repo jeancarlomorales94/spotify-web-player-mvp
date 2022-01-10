633bddd33537ccb94031d0b9fcb7bb558fe729de
Jean Morales <jcm_94@live.com>
2022-01-10 16:01:01 -0500

    added tailwincss

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import spotifyUrl from '../../app/spotifyAuth'
+import { HomeIcon } from '@heroicons/react/solid'
 
 const Login = () => {
     const onLogin = () => {
         window.location.assign(spotifyUrl)
     }
     return (
-        <div>
-            <h2>Login</h2>
-            <button onClick={onLogin}>Login</button>
-        </div>
+        <div className='flex h-screen'>
+            <button className='p-6 m-auto bg-white rounded-xl shadow-md flex items-center space-x-4' onClick={onLogin}>
+                <HomeIcon className="h-12 w-12 text-spotify-color" />
+                <div>
+                    <div className="text-xl font-medium text-black">Spotify Web Player</div>
+                    <p>Click to login into <span className='text-spotify-color'>Spotify</span></p>
+                </div >
+            </button >
+        </div >
     )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'spotify-color': '#1DB954',
+      }
+    },
+  },
+  plugins: [],
+}
